5a8ebb8ebe87b27db603e7f675b323efe3a47af4
Jaydeep <jaydeep.dey03@gmail.com>
2023-04-23 09:15:10 +0530

    ui fixes

--- app/pages/index.tsx
+++ app/pages/index.tsx
@@ -1,6 +1,6 @@
 import type { NextPage } from "next";
 import Navbar from "../Components/Navbar";
-import { Flex, Heading, IconButton, Link, Textarea, VStack, useColorMode } from "@chakra-ui/react";
+import { Box, Flex, Heading, IconButton, Link, Textarea, VStack, useColorMode } from "@chakra-ui/react";
 import { toast, ToastContainer } from 'react-toastify';
 import "react-toastify/dist/ReactToastify.css";
 import { useEffect, useState } from "react";
@@ -36,7 +36,7 @@ const Home: NextPage = () => {
   }
 
   return (
-    <div>
+    <>
       <ToastContainer
         position="top-right"
         autoClose={5000}
@@ -49,53 +49,55 @@ const Home: NextPage = () => {
         pauseOnHover
         theme="light"
       />
+      <Box minH="100vh">
 
-      <Navbar />
-      <VStack p={4} pb={28}>
-        <Addtask addTask={addTask} />
-        
-        <Showtask tasks={tasks} updateTask={updateTask} deleteTask={deleteTask} deleteTaskAll={deleteTaskAll} checkCompletedTask={checkCompletedTask}/>
+        <Navbar />
+        <VStack p={4} pb={28}>
+          <Addtask addTask={addTask} />
 
-        <Flex position='absolute' bottom='5'>
-          <Link href='https://github.com/jaydeepdey03' target='_blank' >
-            <IconButton
-              aria-label="Github"
-              icon={<FaGithub />}
-              rounded={"full"}
-              size='md'
-              m='1'
-            />
-          </Link>
-          <Link href='https://www.linkedin.com/in/jaydeep-dey03/' target='_blank'>
-            <IconButton
-              aria-label="Linkedin"
-              icon={<FaLinkedin />}
-              rounded={"full"}
-              size='md'
-              m='1'
-            />
-          </Link>
-          <Link href='https://instagram.com/jaydeep_dey03' target='_blank'>
-            <IconButton
-              aria-label="Instagram"
-              icon={<FaInstagram />}
-              rounded={"full"}
-              size='md'
-              m='1'
-            />
-          </Link>
-          <Link href='https://twitter.com/jaydeep_dey03' target='_blank'>
-            <IconButton
-              aria-label="Twitter"
-              icon={<FaTwitter />}
-              rounded={"full"}
-              size='md'
-              m='1'
-            />
-          </Link>
-        </Flex>
-      </VStack>
-    </div>
+          <Showtask tasks={tasks} updateTask={updateTask} deleteTask={deleteTask} deleteTaskAll={deleteTaskAll} checkCompletedTask={checkCompletedTask} />
+
+          <Flex position='absolute' bottom='5'>
+            <Link href='https://github.com/jaydeepdey03' target='_blank' >
+              <IconButton
+                aria-label="Github"
+                icon={<FaGithub />}
+                rounded={"full"}
+                size='md'
+                m='1'
+              />
+            </Link>
+            <Link href='https://www.linkedin.com/in/jaydeep-dey03/' target='_blank'>
+              <IconButton
+                aria-label="Linkedin"
+                icon={<FaLinkedin />}
+                rounded={"full"}
+                size='md'
+                m='1'
+              />
+            </Link>
+            <Link href='https://instagram.com/jaydeep_dey03' target='_blank'>
+              <IconButton
+                aria-label="Instagram"
+                icon={<FaInstagram />}
+                rounded={"full"}
+                size='md'
+                m='1'
+              />
+            </Link>
+            <Link href='https://twitter.com/jaydeep_dey03' target='_blank'>
+              <IconButton
+                aria-label="Twitter"
+                icon={<FaTwitter />}
+                rounded={"full"}
+                size='md'
+                m='1'
+              />
+            </Link>
+          </Flex>
+        </VStack>
+      </Box>
+    </>
   );
 };
 
